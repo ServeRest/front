3d888d8477afe7f7f2abefe69fdc1aee2ad299a2
“brunobatista” <brunobatista101@gmail.com>
2020-09-17 19:19:57 -0300

    tela de cadastro

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Router, Switch, Route } from 'react-router-dom';
+import history from './services/history';
+import 'bootswatch/dist/minty/bootstrap.min.css';
+import CadastroUsuario from './views/registerUser';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/cadastrousuarios" component={ CadastroUsuario } />
+          <Route exact path="/" render={() => history.push('/cadastrousuarios')} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/component/alert.js
@@ -0,0 +1,31 @@
+import React,{ Component } from 'react';
+import 'bootswatch/dist/minty/bootstrap.min.css';
+
+export default class SuccessAlert extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: '',
+      display: true
+    }
+  }
+  
+  handleClick= () => {
+    this.setState({
+      display: false
+    })
+  }
+  
+  render() {
+    return (
+      <div>
+        { this.state.display && 
+          <div className="alert alert-dismissible alert-primary">
+              <button type="button" className="close" data-dismiss="alert" onClick={() => this.handleClick()}>&times;</button>
+          <a href="/#" className="alert-link">{this.props.nome}</a>
+          </div>
+        }
+      </div>
+    )
+  }
+}
